6ec59a082f0cc65b265ea78107618ae36cf5ff31
Kengya <mkengya@gmail.com>
2022-01-12 13:46:37 -0500

    creating hooks https request

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import TopMenu from "./components/TopMenu/TopMenu";
+import useFecth from "./hooks/useFetch";
+import { urlApiProducts } from "./utils/constants";
 
 const App =() => {
+
+    const result = useFecth(urlApiProducts);
+    console.log(result);
+
+    
+
   return (
     <div className="App">
       <TopMenu/>
