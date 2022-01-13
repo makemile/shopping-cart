8441c1e3b0da58bdc65fe066f26e046f30b2d647
Kengya <mkengya@gmail.com>
2022-01-13 11:11:17 -0500

    toast

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import TopMenu from "./components/TopMenu/TopMenu";
 import useFecth from "./hooks/useFetch";
 import Products from "./components/products/Products";
+import { ToastContainer, toast } from "react-toastify";
 import { urlApiProducts } from "./utils/constants";
 import { STORAGE_PRODUCTS_CART } from "./utils/constants";
 import { useState } from "react";
@@ -10,20 +11,32 @@ const App = () => {
   const [idProduct, setIdiProduct] = useState([]);
 
   const addProductCart = (id, name) => {
-   const productId = idProduct;
-   productId.push(id);
-   setIdiProduct(productId)
-   localStorage.setItem(STORAGE_PRODUCTS_CART, productId);
+    const productId = idProduct;
+    productId.push(id);
+    setIdiProduct(productId);
+    localStorage.setItem(STORAGE_PRODUCTS_CART, productId);
 
-   console.log("producto añadido")
-    
-  }
- 
+    toast.success(`${name} añadido al carrito`);
+  };
 
   return (
     <div className="App">
       <TopMenu />
       <Products products={products} addProductCart={addProductCart} />
+      <ToastContainer
+      position="bottom-left"
+      autoClose={5000}
+      hideProgressBar
+      newestOnTop
+      closeOnClick={false}
+      rtl={false}
+      pauseOnVisibilityChange={false}
+      draggable
+      pauseOnHover={false}
+
+
+      
+      />
     </div>
   );
 };
